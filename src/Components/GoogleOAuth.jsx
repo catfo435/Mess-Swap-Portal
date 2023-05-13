a2893ca09822dc3c76ecdd0ce413ecaa0b7cbfc8
catfo <pranavlsgreat210120@gmail.com>
2023-05-13 07:19:07 +0530

    Removed async defer from <script>

--- src/Components/GoogleOAuth.jsx
+++ src/Components/GoogleOAuth.jsx
@@ -3,40 +3,27 @@ import GoogleSkeleton from './GoogleSkeleton'
 
 export default function GoogleOAuth(props) {
 
-    const [scriptLoadState,setscriptLoadState] = useState(false)
-    const [skeletonState,setskeletonState] = useState(true)
-
     useEffect(() => {
         /* global google */
 
-        try {
-            google.accounts.id.initialize({
-                client_id: process.env.REACT_APP_CLIENTID,
-                callback: props.onSuccess,
-                hosted_domain: "hyderabad.bits-pilani.ac.in",
-            })
+        google.accounts.id.initialize({
+            client_id: process.env.REACT_APP_CLIENTID,
+            callback: props.onSuccess,
+            hosted_domain: "hyderabad.bits-pilani.ac.in",
+        })
 
-            google.accounts.id.prompt();
+        google.accounts.id.prompt();
 
 
-            google.accounts.id.renderButton(
-                document.getElementById("google-oauth"),
-                { theme: "filled_black", shape: "circle", size: "large", string: "continue_with" }
-            )
-
-        }
-        catch(e){
-            setscriptLoadState(!scriptLoadState) //doesnt actually mean whether the script actually loaded
-                                               // its to trigger the useEffect again
-            return
-        }
-        setskeletonState(false)
-    },[scriptLoadState]);
+        google.accounts.id.renderButton(
+            document.getElementById("google-oauth"),
+            { theme: "filled_black", shape: "circle", size: "large", string: "continue_with" }
+        )
+    }, []);
 
     return (
         <>
-        {skeletonState && <GoogleSkeleton/>}
-        <div id="google-oauth"></div>
+            <div id="google-oauth"></div>
         </>
     )
 }
